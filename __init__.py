6d255ac2af885a54106d33ca435656add41a9af5
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-07-12 11:32:05 -0500

    Adding actual features

---
+++ __init__.py
@@ -0,0 +1,18 @@
+import requests as r
+import json
+import datetime
+
+class MicrosoftHealth:
+  token = ''
+  def __init__(self, token):
+    self.token = 'Bearer ' + token
+
+  def getProfile(self):
+    request = r.get('https://api.microsofthealth.net/v1/me/Profile?', headers={'Authorization': self.token})
+    toReturn = json.loads(request.text)
+    return toReturn
+
+  def getSteps(self):
+    request = r.get('https://api.microsofthealth.net/v1/me/Summaries/daily?', headers={'Authorization': self.token})
+    toReturn = json.loads(request.text)
+    return toReturn
--- index.py
+++
@@ -1,5 +0,0 @@
-import requests as r
-
-request = r.get('https://api.microsofthealth.net/v1/me/Profile?', headers={'Authorization': token});
-
-print request.text
